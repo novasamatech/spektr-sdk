02eba524d7c680dde8f5f5d556fd428ec8093012
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-12-17 01:18:06 +0300

    chore: export identity hooks

--- packages/host-papp-ui/src/index.ts
+++ packages/host-papp-ui/src/index.ts
@@ -1,4 +1,5 @@
 export { useAuthenticateFlow } from './providers/AuthProvider.js';
 export { useSession } from './providers/SessionsProvider.js';
+export { useIdentity, useSessionIdentity } from './hooks/identity.js';
 export { PappProvider } from './flow/PappProvider.js';
 export { PairingModal } from './flow/PairingModal.js';
