e2f223180b6ae027809193473fbcfbdd4a147fc4
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-12-22 19:33:13 +0100

    chore: fixed codec

--- packages/host-papp/src/sso/sessionManager/scale/signPayloadResponse.ts
+++ packages/host-papp/src/sso/sessionManager/scale/signPayloadResponse.ts
@@ -1,11 +1,11 @@
 import type { CodecType } from 'scale-ts';
-import { Bytes, Enum, Struct, str } from 'scale-ts';
+import { Bytes, Struct, str } from 'scale-ts';
 
 export type SignPayloadResponse = CodecType<typeof SignPayloadResponseCodec>;
 export const SignPayloadResponseCodec = Struct({
   // referencing to RemoteMessage.messageId
   respondingTo: str,
-  payload: Enum({
+  payload: Struct({
     signature: Bytes(),
     signedTransaction: Bytes(),
   }),
